cd55adc03704a333b3e07ec4c687c366f7a30a99
jruss <jruss71@gmail.com>
2023-05-18 01:20:00 -0400

    Walking directions and formatting changes,

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WDWPennyFinder;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WDWPennyFinder;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
